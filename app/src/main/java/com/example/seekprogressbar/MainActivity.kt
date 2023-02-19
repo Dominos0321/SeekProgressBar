8e399495b38909814bad70976a61d38a5d8e2bd5
Dominik <dsch792@gmail.com>
2023-02-19 23:08:26 +0100

    Add functionality to SeekBars and ProgressBars

--- app/src/main/java/com/example/seekprogressbar/MainActivity.kt
+++ app/src/main/java/com/example/seekprogressbar/MainActivity.kt
@@ -21,37 +21,106 @@ class MainActivity : AppCompatActivity() {
         val btn = findViewById<Button>(R.id.button)
 
         // width
-        val seekbar1 = findViewById<SeekBar>(R.id.widthseek1)
-        val seekbar2 = findViewById<SeekBar>(R.id.widthseek2)
-        val seekbar3 = findViewById<SeekBar>(R.id.widthseek3)
+        val widthbar1 = findViewById<SeekBar>(R.id.widthseek1)
+        val widthbar2 = findViewById<SeekBar>(R.id.widthseek2)
+        val widthbar3 = findViewById<SeekBar>(R.id.widthseek3)
 
         // height
 
-        val seekbar4 = findViewById<SeekBar>(R.id.heightseek1)
-        val seekbar5 = findViewById<SeekBar>(R.id.heightseek2)
-        val seekbar6 = findViewById<SeekBar>(R.id.heightseek3)
+        val heightbar1 = findViewById<SeekBar>(R.id.heightseek1)
+        val heightbar2 = findViewById<SeekBar>(R.id.heightseek2)
+        val heightbar3 = findViewById<SeekBar>(R.id.heightseek3)
 
 
         // Progess Bar
 
-        val widthBar = findViewById<ProgressBar>(R.id.WBar)
-        val heightBar = findViewById<ProgressBar>(R.id.HBar)
+        val widthBars = findViewById<ProgressBar>(R.id.WBar)
+        val heightBars = findViewById<ProgressBar>(R.id.HBar)
+
+        // Functions width , height
+        fun width() {
+            widthBars.progress = (widthbar1.progress + widthbar2.progress + widthbar3.progress) / 3
+        }
+
+        fun height() {
+            heightBars.progress = (heightbar1.progress + heightbar2.progress + heightbar3.progress) / 3
+        }
 
         // Reset Button
 
         val resetbtn = findViewById<Button>(R.id.button)
 
         resetbtn.setOnClickListener {
-            seekbar1.progress = 100
-            seekbar2.progress = 100
-            seekbar3.progress = 100
-            seekbar4.progress = 100
-            seekbar5.progress = 100
-            seekbar6.progress = 100
+            widthbar1.progress = 100
+            widthbar2.progress = 100
+            widthbar3.progress = 100
+            heightbar1.progress = 100
+            heightbar2.progress = 100
+            heightbar3.progress = 100
 
-            widthBar.progress = 100
-            heightBar.progress = 100
+            widthBars.progress = 100
+            heightBars.progress = 100
 
         }
+
+        // img1
+        widthbar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img1.scaleX = progress.toFloat() / 100f
+                width()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        heightbar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img1.scaleY = progress.toFloat() / 100f
+                height()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        // img2
+        widthbar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img2.scaleX = progress.toFloat() / 100f
+                width()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        heightbar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img2.scaleY = progress.toFloat() / 100f
+                height()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        // img3
+        widthbar3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img3.scaleX = progress.toFloat() / 100f
+                width()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+        heightbar3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                img3.scaleY = progress.toFloat() / 100f
+                height()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
